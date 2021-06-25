0dcda4d0ad088cb4e8b8fc905bd5a3cfc4d34db8
charith-creater <65659313+charith-creater@users.noreply.github.com>
2021-06-25 12:45:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./Fotter.jsx";
+import Header from "./Header.jsx";
+import Info from "./Info.jsx";
+
+function App() {
+  return (
+    <div>
+      <Footer />
+      <Header />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> JavaScript and Node.JS Bootcamp</h1>
+      <p>
+        Hello, this Bootcamp will help you to get some knowledge about Backend
+        web devolopment this is free 7 days live BOOTCAMP with a hands-on
+        project at the end - The Foundation of Javascript and Backend with
+        NodeJS
+        {' {"Shaurya Sinha Will be Teaching."}'}
+      </p>
+      <list>
+        <p># No Prerequiste</p>
+      </list>
+      <list>
+        <p># Free of Cost</p>
+      </list>
+      <list>
+        <p>
+          # Three Certificate{" "}
+          {
+            "(1 for Having 100% Attendance,2 for Completing the Hands-on Project)"
+          }
+        </p>
+      </list>
+      <p> </p>
+      <list>
+        <p>@ Starts from June 27,2021</p>
+      </list>
+    </div>
+  );
+}
+export default Info;
